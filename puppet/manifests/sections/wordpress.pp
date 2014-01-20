cedaad15171333eb8699524a52d4d66494d7164c
Doug Stewart <zamoose@gmail.com>
2014-01-20 15:47:05 -0500

    Doc a bit.

--- puppet/manifests/sections/wordpress.pp
+++ puppet/manifests/sections/wordpress.pp
@@ -8,19 +8,21 @@ $utpath = '/var/www/unittest.dev'
 $wptpath = '/var/www/wptest.dev'
 $mtpath = '/var/www/multitest.dev'
 
-# Install WordPress 1 (Theme Unit Test data)
 trt::install {
+	# Install WordPress 1 (Theme Unit Test data)
 	'unittest.dev':
 		wp_url			=> 'unittest.dev',
 		install_path 	=> $utpath;
 	
+	# Install WordPress 2 (WPTest.io data)
 	'wptest.dev':
 		wp_url			=> 'wptest.dev',
 		install_path 	=> $wptpath,
 		db_user 		=> 'wordpress2',
 		db_pass			=> 'wordpress2',
 		db_name			=> 'wordpress2';
-	
+		
+	# Install WordPress 3 (Multisite)
 	'multitest.dev':
 		wp_url			=> 'multitest.dev',
 		install_path 	=> $mtpath,
@@ -30,34 +32,3 @@ trt::install {
 		multisite		=> true;
 					
 }
-
-# Install WordPress 2 (WPTest.io data)
-
-# Install WordPress 3 (Multisite)
-
-# 
-# exec { "wp download $utpath":
-# 	command 	=> "/usr/bin/wp core download",
-# 	cwd 		=> $utpath,
-# 	unless 		=> "test -f $utpath/wp-config-sample.php",
-# 	require 	=> [ Class['wp::cli'], File[$utpath] ],	
-# }
-# exec {"wp install $utpath":
-# 	command => "/usr/bin/wp core install --base='unittest.dev' --title='unittest.dev' --admin_email='wordpress@unittest.dev' --admin_name='wordpress' --admin_password='wordpress'",
-# 	cwd => $utpath,
-# 	require => [ Class['wp::cli'], File[$utpath], Exec["wp download $utpath."] ],
-# }
-
-# # Install WordPress 2 (WPTest.io data)
-# exec {"wp install /var/www/wptest.dev":
-# 	command => "/usr/bin/wp core install --base='wptest.dev' --title='wptest.dev' --admin_email='wordpress@wptest.dev' --admin_name='wordpress' --admin_password='wordpress'",
-# 	cwd => '/var/www/unittest.dev',
-# 	require => [ Class['wp::cli'], File['/var/www/wptest.dev'] ],
-# }
-# 
-# # Install WordPress 3 (Multisite)
-# exec {"wp install /var/www/multitest.dev":
-# 	command => "/usr/bin/wp core multisite-install --base='multitest.dev' --title='multitest.dev' --admin_email='wordpress@multitest.dev' --admin_name='wordpress' --admin_password='wordpress'",
-# 	cwd => '/var/www/multitest.dev',
-# 	require => [ Class['wp::cli'], File['/var/www/multitest.dev'] ],
-# }
