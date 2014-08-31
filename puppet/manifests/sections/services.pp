dde05b9dadb986a965dfb552c5cf596905a302d1
Doug Stewart <zamoose@gmail.com>
2014-08-31 13:23:15 -0400

    Clean up, make sure FPM is running.

--- puppet/manifests/sections/services.pp
+++ puppet/manifests/sections/services.pp
@@ -1,14 +1,8 @@
-# service { "httpd":
-# 	ensure		=> "running",
-# 	enable		=> "true",
-# }
-
-# service { "mysqld":
-# 	ensure		=> "running",
-# 	enable		=> "true",
-# }
-
-service { "iptables":
-	ensure		=> "stopped",
-	enable		=> "false",
+service {
+	"iptables":
+		ensure		=> "stopped",
+		enable		=> "false";
+	"php-fpm":
+		ensure		=> "running",
+		enable		=> "true";
 }
