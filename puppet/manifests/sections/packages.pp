7940ca3a60f6841887bdf382f0ad94439dfbd81a
Doug Stewart <zamoose@gmail.com>
2014-10-02 00:29:12 -0400

    Add the repos and add resource collectors to make sure all Yumrepos are enabled before trying to install any Packages.

--- puppet/manifests/sections/packages.pp
+++ puppet/manifests/sections/packages.pp
@@ -1,15 +1,22 @@
 # Grab the list of repositories from Hiera
 $repos = hiera_array("repositories")
 # Grab the list of packages from Hiera
-$packages = hiera_array("packages")
+$packages = hiera_array("os-packages")
+
+# alert($repos)
+# alert($packages)
 
 # Set up yum repositories
 class { 'yum':
 	extrarepo	=> $repos,
-}->
+}
 
 # Install package roster
 package { $packages:
 	ensure		=> "latest",
 	provider	=> "yum",
 }
+
+# Require that all Yum repositories have been
+# enabled before trying to add any packages
+Yumrepo <| |> -> Package <| |>
