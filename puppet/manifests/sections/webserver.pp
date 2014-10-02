dd6f779c92705b53cbe7aa1180fdb9ad1c5ef196
Doug Stewart <zamoose@gmail.com>
2014-10-02 00:28:16 -0400

    Nginx configs.

--- puppet/manifests/sections/webserver.pp
+++ puppet/manifests/sections/webserver.pp
@@ -1,1 +1,31 @@
-class { 'nginx': }
+# class { 'nginx': }
+file {
+    '/etc/nginx/global':
+        ensure      => 'directory',
+        owner       => 'root',
+        group       => 'root',
+        mode        => '0755';
+
+    '/etc/nginx/global/wordpress.conf':
+        content     => template('nginx/global/wordpress.conf'),
+        owner       => 'root',
+        group       => 'root',
+        mode        => '0644',
+        require     => File['/etc/nginx/global'],
+        notify      => Service['nginx'];
+
+    '/etc/nginx/global/restrictions.conf':
+        content     => template('nginx/global/restrictions.conf'),
+        owner       => 'root',
+        group       => 'root',
+        mode        => '0644',
+        require     => File['/etc/nginx/global'],
+        notify      => Service['nginx'];
+
+    '/etc/nginx/conf.d/php-upstream.conf':
+        content     => template('nginx/conf.d/php-upstream.conf'),
+        owner       => 'root',
+        group       => 'root',
+        mode        => '0644',
+        notify      => Service['nginx'];
+}
