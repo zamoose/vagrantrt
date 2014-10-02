940cc9a411c5c772c3345eeba0bcfe28276e813d
Doug Stewart <zamoose@gmail.com>
2014-10-02 00:31:56 -0400

    Make sure the correct services are running.

--- puppet/manifests/sections/services.pp
+++ puppet/manifests/sections/services.pp
@@ -1,9 +1,13 @@
 service {
-	"iptables":
-		ensure		=> "stopped",
-		enable		=> "false";
-	"php-fpm":
-		ensure		=> "running",
-		enable		=> "true",
-		require		=> Package['php-fpm'];
+	'iptables':
+		ensure		=> 'stopped',
+		enable		=> 'false';
+	'nginx':
+		ensure		=> 'running',
+		enable		=> 'true';
+	'php54-php-fpm':
+		ensure		=> 'running',
+		enable		=> 'true';
 }
+
+Package <| |> -> Service <| |>
